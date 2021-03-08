b0c2de72f66a1795fbe61bf1b84086038da01186
Louis Iaeger <louis.iaeger@gmail.com>
2021-03-08 12:07:25 -0600

    Make navbar title and icon more responsive on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-      <p className='nav-title'>⚛️ React Starter Template</p>
+      <p className='nav-title'>
+        <span className='nav-title-icon'>⚛️</span> React Starter Template
+      </p>
       <div className='nav-links'>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
