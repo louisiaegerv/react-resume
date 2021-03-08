108bfb90c06fd169d62d86681281af3fc36b2be3
Louis Iaeger <louis.iaeger@gmail.com>
2021-03-08 12:29:11 -0600

    Add link back to home when site title clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-      <p className='nav-title'>
-        <span className='nav-title-icon'>⚛️</span>
-        React Starter Template
-      </p>
+      <Link to='/'>
+        <p className='nav-title'>
+          <span className='nav-title-icon'>⚛️</span>
+          React Starter Template
+        </p>
+      </Link>
       <div className='nav-links'>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
